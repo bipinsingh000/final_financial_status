3928d5861cc986b8e7f576b5084f54df03cba20e
Bipin Singh_1 <www.bipinsingh66@gmail.com>
2026-03-11 22:49:07 +0530

    Refactor input fields for financial ratios

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ st.sidebar.header("Financial Ratios")
 
 #Input fields
 
-f1 = st.sidebar.number_input("Net_profit_total_assets", -0.05, 0.35, 0.10)
-f2 = st.sidebar.number_input("Total_liabilities_total_assets", 0.05, 0.75, 0.40)
-f3 = st.sidebar.number_input("EBIT_total_assets", -0.10, 0.35, 0.15)
-f4 = st.sidebar.number_input("Sales_total_assets", 0.50, 2.00, 1.10)
-f5 = st.sidebar.number_input("Gross_profit_current_liabilities", -0.10, 1.00, 0.35)
-f6 = st.sidebar.number_input("Net_profit_total_liabilities", -0.20, 0.25, 0.05)
-f7 = st.sidebar.number_input("Operating_profit_total_assets2", -0.10, 0.35, 0.15)
-f8 = st.sidebar.number_input("Sales_inventory", 0.50, 10.00, 3.50)
+f1 = st.sidebar.number_input("Net_profit_total_assets" )
+f2 = st.sidebar.number_input("Total_liabilities_total_assets")
+f3 = st.sidebar.number_input("EBIT_total_assets")
+f4 = st.sidebar.number_input("Sales_total_assets")
+f5 = st.sidebar.number_input("Gross_profit_current_liabilities")
+f6 = st.sidebar.number_input("Net_profit_total_liabilities")
+f7 = st.sidebar.number_input("Operating_profit_total_assets")
+f8 = st.sidebar.number_input("Sales_inventory")
 
 #Prediction button
 #Prediction button
